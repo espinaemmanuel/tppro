de131016685be3e074e48a7c1bb1e543c0749a61
ajbazan <ajbazan@gmail.com>
2013-01-26 00:07:35 +0100

    Se modifica el ejemplo de cliente php y se crea una nueva instancia. Contiene un servicio llamado service.php que se utiliza para hacer consultas

---
+++ phpClient/service.php
@@ -0,0 +1,53 @@
+<?php
+namespace tppro;
+
+error_reporting ( E_ALL );
+
+require_once __DIR__ . '/lib/Thrift/ClassLoader/ThriftClassLoader.php';
+require_once __DIR__ . '/REST/RestUtils.php';
+
+use Thrift\ClassLoader\ThriftClassLoader;
+
+$GEN_DIR = realpath ( dirname ( __FILE__ ) ) . '/gen-php';
+
+$loader = new ThriftClassLoader ();
+$loader->registerNamespace ( 'Thrift', __DIR__ . '/lib' );
+$loader->registerDefinition ( 'shared', $GEN_DIR );
+$loader->registerDefinition ( 'tppro', $GEN_DIR );
+$loader->register ();
+
+use Thrift\Protocol\TBinaryProtocol;
+use Thrift\Transport\TSocket;
+use Thrift\Transport\TBufferedTransport;
+use Thrift\Exception\TException;
+
+//Service call
+$data = RestUtils::processRequest();
+
+$data->response=  search($data->request_vars['query']);
+$data->response= json_encode($data->response);
+RestUtils::sendResponse(200, $data->response, 'application/json');
+
+function search($query='world'){
+  try{
+  
+      $socket = new TSocket ( 'localhost', 9090 );
+      $transport = new TBufferedTransport ( $socket, 1024, 1024 );
+      $protocol = new TBinaryProtocol ( $transport );
+    
+      $client = new IndexNodeClient($protocol);
+
+      $transport->open ();
+
+      $results = $client->search(0, $query, 10, 0);
+
+      $transport->close();
+      
+      return $results;
+      
+      } catch ( TException $tx ) {
+          print 'TException: ' . $tx->getMessage () . "\n";
+      }
+}
+
+?>
