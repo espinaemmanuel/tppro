7ee8accecc13962f002d3e1ce30cec2bf3b466c0
emmanuelespina <emmanuelespina@10.0.0.3>
2013-06-09 23:38:54 -0300

    consola mas facil de usar

--- console/src/main/java/ar/uba/fi/tppro/console/Console.java
+++ console/src/main/java/ar/uba/fi/tppro/console/Console.java
@@ -1,10 +1,12 @@
 package ar.uba.fi.tppro.console;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Map;
 import java.util.Set;
+
+import jline.TerminalFactory;
+import jline.console.ConsoleReader;
+import jline.console.completer.StringsCompleter;
 
 import org.apache.thrift.TException;
 import org.reflections.Reflections;
@@ -14,63 +16,78 @@ import org.slf4j.LoggerFactory;
 import com.google.common.collect.Maps;
 
 public class Console {
-	
+
 	private static Map<String, Command> commands = Maps.newHashMap();
 	final static Logger logger = LoggerFactory.getLogger(Console.class);
-
 
 	/**
 	 * @param args
 	 * @throws IOException
-	 * @throws IllegalAccessException 
-	 * @throws InstantiationException 
+	 * @throws IllegalAccessException
+	 * @throws InstantiationException
 	 */
-	public static void main(String[] args) throws IOException, InstantiationException, IllegalAccessException {
-		
-		Reflections reflections = new Reflections("ar.uba.fi.tppro.console.commands");
-	    Set<Class<? extends Command>> subTypes = reflections.getSubTypesOf(Command.class);
-	    
-	    for(Class<? extends Command> command : subTypes){
-	    	Command newCommand = command.newInstance();
-	    	commands.put(newCommand.getName(), newCommand);
-	    }
-	    
-	    Context context = new Context();
-		
-		String curLine = ""; // Line read from standard in
+	public static void main(String[] args) throws IOException,
+			InstantiationException, IllegalAccessException {
 
-		System.out.println("Enter a line of text (type 'quit' to exit): ");
-		InputStreamReader converter = new InputStreamReader(System.in);
-		BufferedReader in = new BufferedReader(converter);
+		Reflections reflections = new Reflections(
+				"ar.uba.fi.tppro.console.commands");
+		Set<Class<? extends Command>> subTypes = reflections
+				.getSubTypesOf(Command.class);
 
-		while (!(curLine.equals("quit"))) {
-			curLine = in.readLine();
-			
-			if(curLine.equals("help")){
-				for(String commandName : commands.keySet()){
-					System.out.println(commandName);
-				}
-				continue;
-			}
-
-			if (!(curLine.equals("quit"))) {
-				String[] parts = curLine.split(" ");
-				
-				if(commands.containsKey(parts[0])){
-					try {
-						commands.get(parts[0]).execute(parts, context);
-					} catch (TException e) {
-						logger.error("Remote exception on command: " + e.getMessage());
-					} catch (Exception e) {
-						logger.error("Exception on command", e);
-					}
-				} else {
-					System.out.println("Command not found");
-				}
-				
-			}
+		for (Class<? extends Command> command : subTypes) {
+			Command newCommand = command.newInstance();
+			commands.put(newCommand.getName(), newCommand);
 		}
 
+		Context context = new Context();
+
+		try {
+			ConsoleReader console = new ConsoleReader();
+			console.setPrompt("tppro> ");
+			console.addCompleter(new StringsCompleter(commands.keySet()));
+			
+			//TOOD: cargar historia desde un archivo
+			console.getHistory().add("sarasa");
+			
+			console.println("Enter a line of text (type 'quit' to exit): ");
+
+			String curLine = ""; // Line read from standard in
+
+			while (!(curLine.equals("quit"))) {
+				curLine = console.readLine();
+
+				if (curLine.equals("help")) {
+					for (String commandName : commands.keySet()) {
+						System.out.println(commandName);
+					}
+					continue;
+				}
+
+				if (!(curLine.equals("quit"))) {
+					String[] parts = curLine.split(" ");
+
+					if (commands.containsKey(parts[0])) {
+						try {
+							commands.get(parts[0]).execute(parts, context);
+						} catch (TException e) {
+							logger.error("Remote exception on command: "
+									+ e.getMessage());
+						} catch (Exception e) {
+							logger.error("Exception on command", e);
+						}
+					} else {
+						System.out.println("Command not found");
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				TerminalFactory.get().restore();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
-	
 }
